368a4cb83cd2d4e1f9d1f57f6c2137f98d394c83
TranzysThor <maxim.akulevich@gmail.com>
2021-10-18 09:15:00 +0300

    add text to img swap and back to top button

--- scripts/swap_script.js
+++ scripts/swap_script.js
@@ -1,9 +1,9 @@
 function swap_to_photo() {
-    let mtkh = document.getElementById("mtkh");
-    mtkh.outerHTML = '<img id="mtkh_img" src="images/mtkh-90.jpg" alt="Тиратрон МТХ 90" height="100" width="100" onclick="swap_to_text()">';
+    let mtkh = document.getElementById("adani");
+    mtkh.outerHTML = '<img id="adani_img" src="images/adani.jpg" alt="гамма-радиометр РУГ-91 «АДАНИ»" height="150" width="100" onclick="swap_to_text()">';
 }
 
 function swap_to_text() {
-    let mtkh_img = document.getElementById("mtkh_img");
-    mtkh_img.outerHTML = '<span id="mtkh" onclick="swap_to_photo()">(тиратрон МТХ 90)</span>';
+    let mtkh_img = document.getElementById("adani_img");
+    mtkh_img.outerHTML = '<span id="adani" onclick="swap_to_photo()">гамма-радиометр РУГ-91 «АДАНИ»</span>';
 }
